c7e61e5ec8e93efa3b53ce17177c9098a7a66119
Rajalakshmi-leema <lakshmirajalakshmi135@gmail.com>
2025-03-06 15:39:41 +0530

    Add files via upload

---
+++ SumOfNum2.java
@@ -0,0 +1,13 @@
+class SumOfNum2
+{
+    public ststic void main(String args[])
+{
+    int sum=0;
+    int n=10;
+    for(int i=1;i<=n;i++)
+    {
+        sum=sum +1;
+    }
+    System.out.println("The sum of"+n+"Numbers are:"+sum);
+}
+}
