53843f3008d8c6f3c5726ef76f57dcc986af65b4
Rajalakshmi-leema <lakshmirajalakshmi135@gmail.com>
2025-03-06 15:16:34 +0530

    Add files via upload

---
+++ SumOfNum.java
@@ -0,0 +1,15 @@
+class SumOfNum
+{
+    public static void main(String args[])
+    {
+        int sum=0;
+        for(int i=1;i<=100;i++)
+        {
+            if(i % 2 !=0)
+            {
+                sum=sum +i;
+            }
+        }
+        System.out.println("The sum of 100 Odd Numbers are:" + sum);
+    }
+}
